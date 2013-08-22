2a66f7a23ba24e1cf6742f4cadb77e257bfc6854
gongmy <gmyao.china@gmail.com>
2013-08-22 19:00:20 +0800

    add2

--- Web/Controllers/SalesMessageController.cs
+++ Web/Controllers/SalesMessageController.cs
@@ -23,13 +23,33 @@ namespace Web.Controllers
             return View(AccountList);
         }
 
-
-        public ActionResult SalesUserList(int id, int accountID, int GruopID)
+        [AllowCheckPermissions(false)]
+        public ActionResult SalesUserList(int? id,int accountID, int GruopID)
         {
+            //数据
             var userModel = Factory.Get<IUserModel>(SystemConst.IOC_Model.UserModel);
-            var UserList = userModel.GetUserByAccountID(accountID, GruopID);
-            return View();
+            var UserList = userModel.GetUserByAccountID(accountID, GruopID).ToPagedList(id??1, 10);
+            //售楼人员姓名
+            var AccountModel = Factory.Get<IAccountModel>(SystemConst.IOC_Model.AccountModel);
+            var account = AccountModel.Get(accountID);
+            ViewBag.AccountName = account.Name;
+            //组名
+            var groupModel = Factory.Get<IGroupModel>(SystemConst.IOC_Model.GroupModel);
+            var group = groupModel.Get(GruopID);
+            ViewBag.GroupName = group.GroupName;
+
+            ViewBag.AccountID = accountID;
+            return View(UserList);
         }
 
+
+        public ActionResult SelectAtoUmes(int ?id ,int accountID,int UserID)
+        {
+            //获取聊天记录
+            var MessageModel = Factory.Get<IMessageModel>(SystemConst.IOC_Model.MessageModel);
+            var message = MessageModel.GetList(accountID, UserID).ToPagedList(id??1, 30);
+            ViewBag.ID = id;
+            return View(message);
+        }
     }
 }
