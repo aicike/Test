d2d0263f2156d2081baba1f5294d806361ecfeb4
issac <176534021@qq.com>
2013-10-10 15:49:41 +0800

    fix bug

---
+++ Poco/WebAPI_Poco/App_ImageText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poco.WebAPI_Poco
+{
+    /// <summary>
+    /// 图文消息
+    /// </summary>
+    public class App_ImageText
+    {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string T { get; set; }
+
+        /// <summary>
+        /// ImagePath
+        /// </summary>
+        public string I { get; set; }
+
+        /// <summary>
+        /// Summary
+        /// </summary>
+        public string S { get; set; }
+
+        /// <summary>
+        /// Content
+        /// </summary>
+        public string C { get; set; }
+
+        public List<App_ImageText> Sub { get; set; }
+    }
+}
--- Web/Controllers/WebRequest_OtherController.cs
+++ Web/Controllers/WebRequest_OtherController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Poco;
+using Injection;
+using Interface;
+using Poco.WebAPI_Poco;
 
 namespace Web.Controllers
 {
@@ -14,6 +17,46 @@ namespace Web.Controllers
             string androidUrl = string.Format("{0}/Download/{1}/android.jpg", SystemConst.WebUrl, amid);
             string iosUrl = string.Format("{0}/Download/{1}/ios.jpg", SystemConst.WebUrl, amid);
             return Newtonsoft.Json.JsonConvert.SerializeObject(new { a = androidUrl, i = iosUrl });
+        }
+
+        /// <summary>
+        /// 获取图文素材
+        /// </summary>
+        /// <param name="amid">AccountMainID</param>
+        public string GetLibraryImageText(int amid)
+        {
+            var libraryImageTextModel = Factory.Get<ILibraryImageTextModel>(SystemConst.IOC_Model.LibraryImageTextModel);
+            var list = libraryImageTextModel.GetLibraryList(amid).ToList();
+            List<App_ImageText> appList = new List<App_ImageText>();
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    App_ImageText app = new App_ImageText();
+                    app.ID = item.ID;
+                    app.I = item.ImagePath;
+                    app.T = item.Title;
+                    app.S = item.Summary;
+                    app.C = item.Content;
+                    if (item.LibraryImageTexts != null && item.LibraryImageTexts.Count > 0)
+                    {
+                        List<App_ImageText> appSubList = new List<App_ImageText>();
+                        foreach (var sub in item.LibraryImageTexts)
+                        {
+                            App_ImageText appSub = new App_ImageText();
+                            appSub.ID = sub.ID;
+                            appSub.I = sub.ImagePath;
+                            appSub.T = sub.Title;
+                            appSub.S = sub.Summary;
+                            appSub.C = sub.Content;
+                            appSubList.Add(appSub);
+                        }
+                        app.Sub = appSubList;
+                    }
+                    appList.Add(app);
+                }
+            }
+            return Newtonsoft.Json.JsonConvert.SerializeObject(appList);
         }
     }
 }
