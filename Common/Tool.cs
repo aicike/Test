afbc4f6976bdeb4d59e85c96890352f012571b6d
issac <176534021@qq.com>
2013-11-22 10:07:16 +0800

    adds

--- Common/Tool.cs
+++ Common/Tool.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
+using System.IO;
 
 namespace Common
 {
@@ -149,7 +150,7 @@ namespace Common
                 //把原始图像绘制成上面所设置宽高的缩小图
                 System.Drawing.Rectangle rectDestination = new System.Drawing.Rectangle(0, 0, thumbWidth, thumbHeight);
                 gr.DrawImage(image, rectDestination, 0, 0, srcWidth, srcHeight, GraphicsUnit.Pixel);
-                bmp.Save(SavePath,bmp.RawFormat);
+                bmp.Save(SavePath, bmp.RawFormat);
                 return true;
             }
             catch (Exception ex)
@@ -217,6 +218,104 @@ namespace Common
             {
                 iSource.Dispose();
                 iSource.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 裁剪，压缩方法
+        /// </summary>
+        /// <param name="imgSrc">原始图片</param>
+        /// <param name="SavePath">保存路径</param>
+        /// <param name="flag">压缩比例，一般设置在70-100之间</param>
+        /// <param name="thumbWidth">缩略图宽度</param>
+        /// <param name="thumbHeight">缩略图高度</param>
+        /// <param name="sm">抗锯齿</param>
+        /// <param name="cq">影像合成</param>
+        /// <param name="im">图片质量</param>
+        /// <returns></returns>
+        public static bool SuperGetPicThumbnail(string imgSrc, string SavePath, int flag, int thumbWidth, int thumbHeight, SmoothingMode sm = SmoothingMode.HighQuality, CompositingQuality cq = CompositingQuality.HighQuality, InterpolationMode im = InterpolationMode.High)
+        {
+            Image image = null;
+            Bitmap bmp = null;
+            System.Drawing.Graphics gr = null;
+            EncoderParameter eParam = null;
+            EncoderParameters ep = null;
+            try
+            {
+                image = System.Drawing.Image.FromFile(imgSrc); //利用Image对象装载源图像
+                //接着创建一个System.Drawing.Bitmap对象，并设置你希望的缩略图的宽度和高度。
+                int srcWidth = image.Width;
+                int srcHeight = image.Height;
+                if (srcWidth < thumbWidth)
+                {
+                    return false;
+                }
+                //int thumbHeight = Convert.ToInt32((srcHeight * 1.0 / srcWidth) * thumbWidth);
+                bmp = new Bitmap(thumbWidth, thumbHeight);
+                //从Bitmap创建一个System.Drawing.Graphics对象，用来绘制高质量的缩小图。
+                gr = System.Drawing.Graphics.FromImage(bmp);
+                //设置 System.Drawing.Graphics对象的SmoothingMode属性为HighQuality
+                gr.SmoothingMode = sm;
+                //下面这个也设成高质量
+                gr.CompositingQuality = cq;
+                //下面这个设成High
+                gr.InterpolationMode = im;
+                //把原始图像绘制成上面所设置宽高的缩小图
+                System.Drawing.Rectangle rectDestination = new System.Drawing.Rectangle(0, 0, thumbWidth, thumbHeight);
+                gr.DrawImage(image, rectDestination, 0, 0, srcWidth, srcHeight, GraphicsUnit.Pixel);
+                ImageFormat tFormat = image.RawFormat;
+                //以下代码为保存图片时，设置压缩质量 
+                ep = new EncoderParameters();
+                long[] qy = new long[1];
+                qy[0] = flag;//设置压缩的比例1-100 
+                eParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, qy);
+                ep.Param[0] = eParam;
+                ImageCodecInfo[] arrayICI = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegICIinfo = null;
+                for (int x = 0; x < arrayICI.Length; x++)
+                {
+                    if (arrayICI[x].FormatDescription.Equals("JPEG"))
+                    {
+                        jpegICIinfo = arrayICI[x];
+                        break;
+                    }
+                }
+                if (jpegICIinfo != null)
+                {
+                    bmp.Save(SavePath, jpegICIinfo, ep);//dFile是压缩后的新路径 
+                }
+                else
+                {
+                    bmp.Save(SavePath, tFormat);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (bmp != null)
+                {
+                    bmp.Dispose();
+                }
+                if (ep != null)
+                {
+                    ep.Dispose();
+                }
+                if (eParam != null)
+                {
+                    eParam.Dispose();
+                }
+                if (gr != null)
+                {
+                    gr.Dispose();
+                }
+                if (image != null)
+                {
+                    image.Dispose();
+                }
             }
         }
     }
