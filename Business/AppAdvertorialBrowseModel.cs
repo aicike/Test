f0a0af9893fa749133b3a0cd9a7abaf0f7e98e23
gongmy <gmyao.china@gmail.com>
2014-03-20 15:38:54 +0800

    adds

---
+++ Business/AppAdvertorialBrowseModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interface;
+using Poco;
+using Poco.Enum;
+
+namespace Business
+{
+    public class AppAdvertorialBrowseModel : BaseModel<AppAdvertorialBrowse> ,IAppAdvertorialBrowseModel
+    {
+
+
+        public Result AddOrUpdBrowseCnt(int ID,EnumBrowseType EBT)
+        {
+            Result result = new Result();
+            switch (EBT)
+            { 
+                //资讯
+                case EnumBrowseType.Consultation:
+                    break;
+                //活动
+                case EnumBrowseType.Activity:
+                    break;
+                //调查
+                case EnumBrowseType.Survey:
+                    break;
+            }
+
+
+
+            return result;
+        }
+    }
+}
---
+++ Poco/Enum/EnumBrowseType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poco.Enum
+{
+    public enum EnumBrowseType
+    {
+        /// <summary>
+        /// 咨询
+        /// </summary>
+        Consultation,
+        /// <summary>
+        /// 活动
+        /// </summary>
+        Activity,
+        /// <summary>
+        /// 调查
+        /// </summary>
+        Survey
+    }
+}
