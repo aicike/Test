030c95339290443b39897336c1a37a7a5f575064
aicike <176534021@qq.com>
2014-05-21 18:08:41 +0800

    Merge remote-tracking branch 'origin/123' into wsq2
    Conflicts:
    	EF/Initial.sql

---
+++ Poco/WebAPI_Poco/App_TakeOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poco.WebAPI_Poco
+{
+    public class App_TakeOut
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImgPath { get; set; }
+
+        public string Summary { get; set; }
+
+        //总价
+        public decimal Price { get; set; }
+
+        //送餐费
+        public decimal TakeOutPrice { get; set; }
+    }
+}
--- Web/ueditor/ueditor.config.js
+++ Web/ueditor/ueditor.config.js
@@ -183,7 +183,7 @@
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
 
         //, toolbars: [["fontfamily", "fontsize", "bold", "italic", "underline", "insertorderedlist", "insertunorderedlist", "insertimage", "removeformat", "forecolor", "backcolor", "insertvideo"]]
-        , toolbars: [["bold", "italic", "underline",  "insertorderedlist", "insertunorderedlist", "insertimage", "removeformat", "forecolor", "backcolor", "insertvideo"]]
+        , toolbars: [["bold", "italic", "underline", "insertorderedlist", "insertunorderedlist", "insertimage", "removeformat", "forecolor", "backcolor", "insertvideo", 'link', 'unlink']]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
 //        ,labelMap:{
 //            'anchor':'', 'undo':''
@@ -412,7 +412,7 @@
 
         //autoHeightEnabled
         // 是否自动长高,默认true
-        //,autoHeightEnabled:true
+        ,autoHeightEnabled:false
 
         //scaleEnabled
         //是否可以拉伸长高,默认true(当开启时，自动长高失效)
